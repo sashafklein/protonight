ece271d8f97547b49dabfe1d205fec9304771a24
Arnaud Frugier <frugier.arnaud@gmail.com>
2013-02-11 21:14:18 -0800

    Google maps

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,28 @@
+
+
 function VegetarianController($scope, $http) {
-  $http.get('data/vegetarian.json').success(function(data) {
-    $scope.vegetarians = data;
-  });
+    
+    var mapOptions = {
+        center: new google.maps.LatLng(-34.387, 150.644),
+        zoom: 8,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map_canvas"),
+        mapOptions);
+    
+    $http.get('data/vegetarian.json').success(function(data) {
+        $scope.vegetarians = data;
+        
+        for(i in data)
+        {
+            var marker = new google.maps.Marker({
+                map:map,
+                draggable:true,
+                animation: google.maps.Animation.DROP,
+                position: new google.maps.LatLng(data[i].latitude, data[i].longitude)
+            });
+        }
+    });
 }
- 
+
 //PhoneListCtrl.$inject = ['$scope', '$http'];
